e5c2cefb85c51cf222bb11e119875d663502902f
OthmanSEBTI <seb.othmane@gmail.com>
2022-11-22 16:11:33 -0500

    adjust Main_slave_setup file

--- Cluster_setup/Main_slave_setup.sh
+++ Cluster_setup/Main_slave_setup.sh
@@ -1,2 +1,2 @@
-sudo bash ~/LOG8415_FinalProject/Cluster_setup/Common_steps.sh
-sudo bash ~/LOG8415_FinalProject/Cluster_setup/Slave_setup.sh
+sudo bash /home/ubuntu/LOG8415_FinalProject/Cluster_setup/Common_steps.sh
+sudo bash /home/ubuntu/LOG8415_FinalProject/Cluster_setup/Slave_setup.sh
