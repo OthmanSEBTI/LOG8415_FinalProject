32caafad2250515dbccb81587222d098ff4d1f13
OthmanSEBTI <seb.othmane@gmail.com>
2022-12-18 10:48:49 -0500

    test.sh modification

--- Standalone_setup/test.sh
+++ Standalone_setup/test.sh
@@ -1,9 +1,7 @@
+# install mysql
 sudo apt-get install mysql-server -y
 # sudo mysql
 
 # install sakila
-
-cd .. ; cp /home/ubuntu/LOG8415_FinalProject/sakila-db.tar.gz /home/ubuntu/
-cd
+cp ~/LOG8415_FinalProject/sakila-db.tar.gz ~/
 tar xvf sakila-db.tar.gz
-sudo apt-get install sysbench -y
