4a412aa562c0ce580f3fd73bef2c915abb2a49bb
OthmanSEBTI <seb.othmane@gmail.com>
2022-12-18 17:27:07 -0500

    Slave_setup update ip adresses

--- Cluster_setup/Slave_setup.sh
+++ Cluster_setup/Slave_setup.sh
@@ -1,2 +1,2 @@
 sudo mkdir -p /opt/mysqlcluster/deploy/ndb_data
-sudo /opt/mysqlcluster/home/mysqlc/bin/ndbd -c ip-172-31-90-158.ec2.internal:1186
+sudo /opt/mysqlcluster/home/mysqlc/bin/ndbd -c ip-172-31-89-21.ec2.internal:1186
--- Main.py
+++ Main.py
@@ -19,7 +19,8 @@ instances= ['mysql_standalone','mysql_cluster_master','mysql_cluster_slave1','my
 for instance in instances :
     create_instance(instance,securityGroupId,key_name)
 
-
+"""
+"""
 key_name = 'key' 
 
 instances= ['mysql_standalone1','mysql_cluster_master','mysql_cluster_slave1','mysql_cluster_slave2','mysql_cluster_slave3']
@@ -39,8 +40,9 @@ sessions['mysql_standalone1'].exec_command("sudo mysql < /home/ubuntu/LOG8415_Fi
 stdin, stdout, stderr =sessions['mysql_standalone1'].exec_command("sudo sysbench oltp_read_write --table-size=1000000 --mysql-db=sakila --mysql-user=root prepare && sudo sysbench oltp_read_write --table-size=1000000 --threads=6 --time=60 --max-requests=0 --mysql-db=sakila --mysql-user=root run")
 for lines in stdout.readlines() :
     print(lines)
-
 """
+
+'''
 key_name = 'key' 
 
 instance= 'mysql_cluster_master'
@@ -52,7 +54,27 @@ instanceId = retrieve_intanceId(instance)
 publicIp = retrieve_publicIp(instanceId)
 sessions[instance] = ssh_to_instance(key_name+'.pem',publicIp)
 
-sessions['mysql_cluster_master'].exec_command("sudo apt-get update")
-stdin, stdout, stderr =sessions['mysql_cluster_master'].exec_command("sudo git clone https://github.com/OthmanSEBTI/LOG8415_FinalProject.git && sudo bash /home/ubuntu/LOG8415_FinalProject/Cluster_setup/Common_steps.sh")
+#sessions['mysql_cluster_master'].exec_command("sudo apt-get update")
+#stdin, stdout, stderr =sessions['mysql_cluster_master'].exec_command("sudo git clone https://github.com/OthmanSEBTI/LOG8415_FinalProject.git && sudo bash /home/ubuntu/LOG8415_FinalProject/Cluster_setup/Common_steps.sh")
+stdin, stdout, stderr =sessions['mysql_cluster_master'].exec_command(" sudo bash /home/ubuntu/LOG8415_FinalProject/Cluster_setup/Master_setup.sh")
+
+print('stdout:', stdout.read())
+print('stderr:', stderr.read())
+
+'''
+key_name = 'key' 
+
+instance= 'mysql_cluster_slave2'
+
+sessions={}
+
+
+instanceId = retrieve_intanceId(instance)
+publicIp = retrieve_publicIp(instanceId)
+sessions[instance] = ssh_to_instance(key_name+'.pem',publicIp)
+
+sessions['mysql_cluster_slave2'].exec_command("sudo apt-get update")
+stdin, stdout, stderr =sessions['mysql_cluster_slave2'].exec_command("  sudo bash /home/ubuntu/LOG8415_FinalProject/Cluster_setup/Slave_setup.sh")
+
 print('stdout:', stdout.read())
 print('stderr:', stderr.read())
