bdc5fda20af25435dab343de694d606762cb9d4a
OthmanSEBTI <seb.othmane@gmail.com>
2022-12-22 11:29:05 -0500

    proxy test

--- Main.py
+++ Main.py
@@ -44,7 +44,7 @@ stdin, stdout, stderr =sessions['mysql_cluster_master'].exec_command("/opt/mysql
 print('stdout:', stdout.read())
 print('stderr:', stderr.read())
 
-'''
+
 #install sysbench in the cluster instances
 for instance in instances[2:]:
     stdin, stdout, stderr =sessions[instance].exec_command("sudo apt-get install sysbench -y")
@@ -52,3 +52,29 @@ for instance in instances[2:]:
     print('stderr:', stderr.read())
 
 
+
+#install sakila db in the master
+stdin, stdout, stderr =sessions['mysql_cluster_master'].exec_command("cp /home/ubuntu/LOG8415_FinalProject/sakila-db.tar.gz /home/ubuntu/ & sudo tar xvf /home/ubuntu/sakila-db.tar.gz")
+print('stdout:', stdout.read())
+print('stderr:', stderr.read())
+
+stdin, stdout, stderr =sessions['mysql_cluster_master'].exec_command("/opt/mysqlcluster/home/mysqlc/bin/mysql -h 127.0.0.1 -u root < /home/ubuntu/LOG8415_FinalProject/Cluster_setup/Install_sakila.sql")
+print('stdout:', stdout.read())
+print('stderr:', stderr.read())
+
+
+#Launch Benchmark
+stdin, stdout, stderr =sessions['mysql_cluster_slave1'].exec_command("sudo sysbench  oltp_read_write --num-threads=16 --max-requests=10000 --db-driver=mysql --mysql-host=ec2-18-234-103-147.compute-1.amazonaws.com --mysql-user=myapp2 --mysql-db=sakila --table-size=1000000 --max-requests=1000000  prepare & time sudo sysbench  oltp_read_write --num-threads=16 --max-requests=10000 --db-driver=mysql --mysql-host=ec2-18-234-103-147.compute-1.amazonaws.com --mysql-user=myapp2 --mysql-db=sakila --table-size=1000000 --max-requests=1000000  run")
+print('stdout:', stdout.read())
+print('stderr:', stderr.read())
+
+'''
+
+# Proxy setup and launch
+sessions['mysql_standalone'].exec_command("sudo apt-get update")
+stdin, stdout, stderr =sessions['mysql_standalone'].exec_command("sudo git clone https://github.com/OthmanSEBTI/LOG8415_FinalProject.git && sudo bash /home/ubuntu/LOG8415_FinalProject/Proxy_setup.sh ")
+print('stdout:', stdout.read())
+print('stderr:', stderr.read())
+stdin, stdout, stderr =sessions['mysql_standalone'].exec_command("python3 ./LOG8415_FinalProject/Proxy_app.py")
+print('stdout:', stdout.read())
+print('stderr:', stderr.read())
