615386c75c613e91733513d5fba9a216bed8cd60
OthmanSEBTI <seb.othmane@gmail.com>
2022-12-23 10:31:49 -0500

    test4

--- Cluster_setup/Slave_setup.sh
+++ Cluster_setup/Slave_setup.sh
@@ -1,2 +1,2 @@
 sudo mkdir -p /opt/mysqlcluster/deploy/ndb_data
-sudo /opt/mysqlcluster/home/mysqlc/bin/ndbd -c ec2-54-211-138-129.compute-1.amazonaws.com:1186
+sudo /opt/mysqlcluster/home/mysqlc/bin/ndbd -c ec2-54-211-94-227.compute-1.amazonaws.com:1186
