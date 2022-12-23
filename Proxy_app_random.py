c90f514ffd6ec050fc96ff57d2a0da227434fa2c
OthmanSEBTI <seb.othmane@gmail.com>
2022-12-23 13:47:34 -0500

    proxy random

--- Proxy_app_random.py
+++ Proxy_app_random.py
@@ -1,18 +1,21 @@
 from Proxy_sessions_setup import sessions
+import random
 
 Client_request = open('/home/ubuntu/request.sql', mode="r", encoding="utf-8")
 # Client_request=sessions['mysql_cluster_master'].exec_command('sudo echo /home/ubuntu/request.sql')
 
 request = Client_request.readlines()[0]
 
-instances= ['mysql_cluster_master','mysql_cluster_slave1','mysql_cluster_slave2','mysql_cluster_slave3']
+instances= ['mysql_cluster_slave1','mysql_cluster_slave2','mysql_cluster_slave3']
 
-cmd = " sudo echo '" + str(request) + "' >> requests.sql && sudo /opt/mysqlcluster/home/mysqlc/bin/mysql -h 127.0.0.1 -u root < requests.sql"
+random_slave=random.choice(instances)
+
+cmd = " sudo echo '" + str(request) + "' >> requests.sql && sudo /opt/mysqlcluster/home/mysqlc/bin/mysql -h ec2-3-82-194-4.compute-1.amazonaws.com -u myapp2 < requests.sql"
 
 
-stdin, stdout, stderr = sessions['mysql_cluster_master'].exec_command(cmd )
+stdin, stdout, stderr = sessions[random_slave].exec_command(cmd )
 print('stdout:', stdout.read())
 print('stderr:', stderr.read())
 
-sessions['mysql_cluster_master'].exec_command("sudo rm requests.sql" )
+sessions[random_slave].exec_command("sudo rm requests.sql" )
 
